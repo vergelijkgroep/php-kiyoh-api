ef618b6a0c2a9db7767fdbb132583ebd35592fad
Jan Goessens <jangoessens@hotmail.com>
2024-12-16 11:57:32 +0100

    add option to set/get fallback data

--- src/Keuze/Kiyoh/Kiyoh.php
+++ src/Keuze/Kiyoh/Kiyoh.php
@@ -8,12 +8,15 @@ use Exception;
 use GuzzleHttp\Client;
 use Keuze\Kiyoh\Factory\ReviewFactory;
 use Keuze\Kiyoh\Model\Company;
+use Keuze\Kiyoh\Model\Review;
+use Keuze\Kiyoh\Model\ReviewContent;
 
 class Kiyoh
 {
     public const COMPANY_REVIEWS_URL = 'https://www.kiyoh.com/v1/review/feed.xml?hash=%s&limit=%s';
 
     private Client $client;
+    private Company|null $fallback;
 
     /**
      * Kiyoh constructor.
@@ -63,5 +66,64 @@ class Kiyoh
     public function getCompanyURL(): string
     {
         return sprintf(self::COMPANY_REVIEWS_URL, $this->connectorCode, $this->reviewCount);
+    }
+
+    /*
+     * Set the fallback company information.
+     * 
+     * @param array $data
+     */
+    public function setFallbackCompany(array $data): void
+    {
+        $company = new Company(
+            (float) ($data['averageRating'] ?? 0.0),
+            (int) ($data['numberReviews'] ?? 0),
+            (float) ($data['last12MonthAverageRating'] ?? 0.0),
+            (int) ($data['last12MonthNumberReviews'] ?? 0),
+            (int) ($data['percentageRecommendation'] ?? 0),
+            (int) ($data['locationId'] ?? 0),
+            locationName: $data['locationName'] ?? ''
+        );
+
+        $reviews = [];
+
+        if (!array_key_exists('reviews', $data)) {
+            $data['reviews'] = [];
+        }
+        foreach ($data['reviews'] as $fallBackReview) {
+            $review = new Review(
+                (string) ($fallBackReview['id'] ?? ''),
+                (string) ($fallBackReview['reviewAuthor'] ?? ''),
+                (string) ($fallBackReview['city'] ?? ''),
+                (float) ($fallBackReview['rating'] ?? 0.0),
+                (string) ($fallBackReview['comment'] ?? ''),
+                (string) ($fallBackReview['dateSince'] ?? ''),
+                (string) ($fallBackReview['updatedSince'] ?? ''),
+                (string) ($fallBackReview['referenceCode'] ?? '')
+            );
+
+            $review->setContent(
+                [
+                    new ReviewContent(
+                        (string) ($fallBackReview['questionGroup'] ?? 'DEFAULT_OPINION'),
+                        (string) ($fallBackReview['questionType'] ?? 'TEXT'),
+                        (string) ($fallBackReview['content'] ?? ''),
+                        (int) ($fallBackReview['order'] ?? 0),
+                        (string) ($fallBackReview['questionTranslation'] ?? '')
+                    )
+                ],
+            );
+
+            $reviews[] = $review;
+        }
+
+        $company->setReviews($reviews);
+
+        $this->fallback = $company;
+    }
+
+    public function getFallbackCompany(): Company|null
+    {
+        return $this->fallback ?? null;
     }
 }
